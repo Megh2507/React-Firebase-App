dfd7e1ac37316fe0b17483d88c278f9ac9a2c208
Megha Paul <megha25072001@gmail.com>
2021-08-02 20:14:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react'
+import './App.css';
+import Reactform from "./Components/Reactform";
+
+function App() {
+  return (
+    <div className="App">
+      <Reactform/>
+    </div>
+  );
+}
+
+export default App;
